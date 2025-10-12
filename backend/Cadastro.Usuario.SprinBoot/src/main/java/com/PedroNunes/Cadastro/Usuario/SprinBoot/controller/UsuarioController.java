49254fde3b474299f2cd6c80f1bced9234f49004
Pedro Nunes <pedrovitornunes89@gmail.com>
2025-10-11 22:47:59 -0300

    Atualizando anotacao feita, para outra mais apropiada ao projeto

--- backend/Cadastro.Usuario.SprinBoot/src/main/java/com/PedroNunes/Cadastro/Usuario/SprinBoot/controller/UsuarioController.java
+++ backend/Cadastro.Usuario.SprinBoot/src/main/java/com/PedroNunes/Cadastro/Usuario/SprinBoot/controller/UsuarioController.java
@@ -3,17 +3,17 @@ package com.PedroNunes.Cadastro.Usuario.SprinBoot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.PedroNunes.Cadastro.Usuario.SprinBoot.model.Usuario;
 import com.PedroNunes.Cadastro.Usuario.SprinBoot.service.UsuarioService;
 
 @CrossOrigin(origins="http://localhost:5500")
-@Controller
+@RestController
 @RequestMapping("/cadastro")
 public class UsuarioController {
 
